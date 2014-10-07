958b2fcbf06dce72fc11fa01b5ad527202ea9aff
Sangyoun Kim <skim@sugarcrm.com>
2014-10-07 02:46:57 -0700

    Adds dependency: RequireJS

---
+++ app/scripts/config.js
@@ -0,0 +1,12 @@
+require.config({
+  // make components more sensible
+  // expose jquery
+  paths: {
+    'components': '../bower_components',
+    'jquery': '../bower_components/jquery/dist/jquery'
+  }
+});
+
+if (!window.requireTestMode) {
+  require(['main'], function() { });
+}
---
+++ app/scripts/main.js
@@ -0,0 +1,3 @@
+define(['jquery'], function($) {
+    $('body').append('jQuery ' + $.fn.jquery + ' loaded!');
+});
